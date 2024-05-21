59d97bfda3b70e06fe9468b47036125fd4b60932
traktor123 <vladimirfedorov3@gmail.com>
2024-05-21 08:53:04 +0300

    React DOM

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+const title = 'React';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+)
+
+ReactDOM.createRoot(document.getElementById('root1')!).render(
+  <h1>{title}</h1>
 )
