43ab8a0efdc648b820cb08be4061fc5a6bb23a41
traktor123 <vladimirfedorov3@gmail.com>
2024-05-21 10:28:22 +0300

    React Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,36 @@
 import * as React from 'react';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+const App = () => {
 
-const App = () => (
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
+  return (
   <div>
     <h1>My Hacker Stories</h1>
     <Search />
     <hr />
-    <List />
+    <List  list={stories}/>
     <hr />
-    <List />
+    <List  list={stories}/>
   </div>
-);
+)};
 
 const Search = () => {
 
@@ -47,20 +49,26 @@ const Search = () => {
   )
 }
 
-const List = () => (
+const List = (props: any) => (
   <ul>
-    {list.map((item) => (
-      <li key={item.objectID}>
-        <span>
-          <a href={item.url}>{item.title}</a>
-        </span>
-        <span>{item.author}</span>
-        <span>{item.num_comments}</span>
-        <span>{item.points}</span>
-      </li>
-    )
-    )}
+    {
+      props.list.map((item: any) => (
+      <Item key={item.objectID} item={item}/>
+      ))
+    }
   </ul>
 );
+
+const Item = (props: any) => 
+  (
+    <li key={props.item.objectID}>
+      <span>
+        <a href={props.item.url}>{props.item.title}</a>
+      </span>
+      <span>{props.item.author}</span>
+      <span>{props.item.num_comments}</span>
+      <span>{props.item.points}</span>
+    </li>
+  )
 
 export default App;
