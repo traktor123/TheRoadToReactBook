c7448268a5f247a2ae9eddb69d1d748873ae7afe
traktor123 <vladimirfedorov3@gmail.com>
2024-05-27 10:51:12 +0300

    React Custom Hooks (Advanced)

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ type ItemProps = {
 }
 
 type SearchProps = {
-  onSearch: (event: React.ChangeEvent<HTMLInputElement>) => void;  
+  onSearch: (event: React.ChangeEvent<HTMLInputElement>) => void;
   searchTerm: string;
 };
 
@@ -61,46 +61,45 @@ const stories: Story[] = [
     num_comments: 2,
     points: 5,
     objectID: 3,
-  },    
+  },
 ];
 
-let someSimpleVar: number = 0;
+const useStorageState = (key:string, initialState: string): [string, React.Dispatch<React.SetStateAction<string>>] => {
+  const [value, setValue] = React.useState(localStorage.getItem(key) || initialState);
 
-const App = () => {  
-  let searchTermStored: string = localStorage.getItem('searchTerm') || 'React';
+  React.useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [value, key]);
 
-  const [searchTerm, setSearchTerm]:[string, React.Dispatch<React.SetStateAction<string>>] = React.useState(searchTermStored);  
+  return [value, setValue];
+};
+
+const App = () => {
+
+  const [searchTerm, setSearchTerm] = useStorageState('searchTerm', 'React');
 
   let filteredStories = searchTerm == ''
-  ? stories
-  : stories.filter(s => s.title.toLowerCase().includes(searchTerm?.toLowerCase() ?? "")); 
+    ? stories
+    : stories.filter(s => s.title.toLowerCase().includes(searchTerm?.toLowerCase() ?? ""));
 
-  React.useEffect(() => {
-    console.log("somesimpleVar = " + someSimpleVar);
-    }, [someSimpleVar]);
-
-  React.useEffect(() => {
-    localStorage.setItem('searchTerm', searchTerm);
-    someSimpleVar++;
-    }, [searchTerm]);
-
-  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {    
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value);
 
     console.log("Log from App Component" + event.target.value);
   };
 
   return (
-  <div>
-    <h1>My Hacker Stories</h1>
-    <Search onSearch={handleSearch} searchTerm={searchTerm}/>
-    <hr />
-    <List  list={filteredStories}/>
-  </div>
-)};
+    <div>
+      <h1>My Hacker Stories</h1>
+      <Search onSearch={handleSearch} searchTerm={searchTerm} />
+      <hr />
+      <List list={filteredStories} />
+    </div>
+  )
+};
 
-const Search:React.FC<SearchProps> = ({ searchTerm, onSearch }) => {  
-  const handleChange:React.EventHandler<React.ChangeEvent> = (event: React.ChangeEvent<HTMLInputElement>) => {
+const Search: React.FC<SearchProps> = ({ searchTerm, onSearch }) => {
+  const handleChange: React.EventHandler<React.ChangeEvent> = (event: React.ChangeEvent<HTMLInputElement>) => {
     onSearch(event);
 
     // synthetic event
@@ -112,34 +111,34 @@ const Search:React.FC<SearchProps> = ({ searchTerm, onSearch }) => {
   return (
     <div>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={handleChange} value={searchTerm}/>
+      <input id="search" type="text" onChange={handleChange} value={searchTerm} />
       <p>
         Searching for <strong>{searchTerm}</strong>
-      </p>      
+      </p>
     </div>
   )
 }
 
-const List:React.FC<ListProps> = ({list}) => (
+const List: React.FC<ListProps> = ({ list }) => (
   <ul>
     {
       list.map((item) => (
-      <Item key={item.objectID} {...item} />
-     ))
+        <Item key={item.objectID} {...item} />
+      ))
     }
   </ul>
 );
 
-const Item: React.FC<ItemProps> = ({ title, url, author, num_comments, points }) => 
-  (
-    <li>
-      <span>
-        <a href={url}>{title}</a>
-      </span>
-      <span>{author}</span>
-      <span>{num_comments}</span>
-      <span>{points}</span>
-    </li>
-  )
+const Item: React.FC<ItemProps> = ({ title, url, author, num_comments, points }) =>
+(
+  <li>
+    <span>
+      <a href={url}>{title}</a>
+    </span>
+    <span>{author}</span>
+    <span>{num_comments}</span>
+    <span>{points}</span>
+  </li>
+)
 
 export default App;
